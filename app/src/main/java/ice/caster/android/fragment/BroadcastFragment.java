df56d1b4c9948f0711e55a11d0b447178e67ae96
Elkanaany <elkanaany.21@gmail.com>
2020-07-10 21:16:24 +0100

    third commit

--- app/src/main/java/ice/caster/android/fragment/BroadcastFragment.java
+++ app/src/main/java/ice/caster/android/fragment/BroadcastFragment.java
@@ -1,6 +1,7 @@
 package ice.caster.android.fragment;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -9,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,28 +28,26 @@ import static android.app.Activity.RESULT_OK;
 public class BroadcastFragment extends Fragment {
     public final static int CHOOSE_BUTTON_REQUEST = 0;
     private Button config;
-    static final String ICE_HOST    = "188.114.110.70";
-
-
-    static final int    ICE_PORT    = 8000;
-
-
-    static final String ICE_MOUNT   = "/ayyoub.ogg";
-
-
-    static final String ICE_USER    = "ayyoub";
-    static final String ICE_PASS    = "ayyoub";
+    public static  String ICE_HOST = "192.168.137.1" ;
+    public static  int    ICE_PORT = 8000 ;
+    public static  String ICE_MOUNT ="/icecast.ogg";
+    public static  String ICE_USER ="source";
+    public static  String ICE_PASS = "ayyoub";
 
     @InjectView(R.id.start)
     Button start;
+    @InjectView(R.id.progressBar)
+    ProgressBar progressBar;
     @InjectView(R.id.stop)
     Button stop;
     @InjectView(R.id.status)
     TextView status;
     @InjectView(R.id.configuration)
     Button configuration;
-
+    @InjectView(R.id.parent)
+    LinearLayout linearLayout;
     private Encoder encoder;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,29 +56,28 @@ public class BroadcastFragment extends Fragment {
                 new Config().host(ICE_HOST).port(ICE_PORT).mount(ICE_MOUNT)
                         .username(ICE_USER).password(ICE_PASS).sampleRate(8000));
 
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_broadcast, container, false);
+
+
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         ButterKnife.inject(this, view);
-
-        /**
-         * This is short-hand setter and handler should be static class
-         */
         encoder.setHandle(new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 switch (msg.what) {
                     case Encoder.MSG_REC_STARTED:
                         status.setText("Streaming");
+                        progressBar.setIndeterminate(true);
                         break;
                     case Encoder.MSG_REC_STOPPED:
                         status.setText("");
@@ -87,26 +87,31 @@ public class BroadcastFragment extends Fragment {
                         Toast.makeText(getActivity(),
                                 "MSG_ERROR_GET_MIN_BUFFERSIZE",
                                 Toast.LENGTH_LONG).show();
+                        progressBar.setIndeterminate(false);
                         break;
                     case Encoder.MSG_ERROR_REC_START:
                         status.setText("");
                         Toast.makeText(getActivity(), "Can not start recording",
                                 Toast.LENGTH_LONG).show();
+                        progressBar.setIndeterminate(false);
                         break;
                     case Encoder.MSG_ERROR_AUDIO_RECORD:
                         status.setText("");
                         Toast.makeText(getActivity(), "Error audio record",
                                 Toast.LENGTH_LONG).show();
+                        progressBar.setIndeterminate(false);
                         break;
                     case Encoder.MSG_ERROR_AUDIO_ENCODE:
                         status.setText("");
                         Toast.makeText(getActivity(), "Error audio encode",
                                 Toast.LENGTH_LONG).show();
+                        progressBar.setIndeterminate(false);
                         break;
                     case Encoder.MSG_ERROR_STREAM_INIT:
                         status.setText("");
                         Toast.makeText(getActivity(), "Can not init stream",
                                 Toast.LENGTH_LONG).show();
+                        progressBar.setIndeterminate(false);
                         break;
                     default:
                         break;
@@ -118,18 +123,27 @@ public class BroadcastFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), ice.caster.android.view.configuration.class);
                 startActivityForResult(intent,CHOOSE_BUTTON_REQUEST);
+                progressBar.setIndeterminate(false);
             }
         });
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 encoder.start();
+                linearLayout.setBackground(getResources().getDrawable(R.drawable.download));
+                start.setVisibility(View.GONE);
+                configuration.setVisibility(View.GONE);
+
             }
         });
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 encoder.stop();
+                linearLayout.setBackground(getResources().getDrawable(R.drawable.border2));
+                start.setVisibility(View.VISIBLE);
+                configuration.setVisibility(View.VISIBLE);
+                progressBar.setIndeterminate(false);
             }
         });
     }
@@ -137,13 +151,11 @@ public class BroadcastFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CHOOSE_BUTTON_REQUEST) {
             if (resultCode == RESULT_OK) {
-              Toast.makeText(getActivity(),"text : "+data.getStringExtra("serveur"),Toast.LENGTH_SHORT).show();
-              Toast.makeText(getActivity(),"text : "+data.getStringExtra("port"),Toast.LENGTH_SHORT).show();
-              Toast.makeText(getActivity(),"text : "+data.getStringExtra("mount"),Toast.LENGTH_SHORT).show();
-                Toast.makeText(getActivity(),"text : "+data.getStringExtra("user"),Toast.LENGTH_SHORT).show();
-              Toast.makeText(getActivity(),"text : "+data.getStringExtra("password"),Toast.LENGTH_SHORT).show();
-
-
+              ICE_HOST =data.getStringExtra("serveur");
+              ICE_PORT = Integer.parseInt(data.getStringExtra("port")) ;
+              ICE_MOUNT = data.getStringExtra("mount");
+              ICE_USER = data.getStringExtra("user") ;
+              ICE_PASS = data.getStringExtra("password");
             }
         }
     }
